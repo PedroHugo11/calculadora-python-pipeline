ad6fd2d186a3d0e4d9afd4c6f1b2050947aaa99b
Pedro Hugo <pedrohuugoo11@hotmail.com>
2021-09-20 21:56:15 -0300

    #01 - teste

---
+++ main.py
@@ -0,0 +1,10 @@
+opcao=1
+
+while opcao:
+    print("0. Sair")
+    print("1. Somar")
+    print("2. Subtrair")
+    print("3. Multiplicação")
+    print("4. Divisão ")
+
+    opcao = int(input("Opção: "))
