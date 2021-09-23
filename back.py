51fc38ae7a11cce1b2d1404604d41778956eb478
Pedro Hugo <pedrohuugoo11@hotmail.com>
2021-09-22 23:47:11 -0300

    #04 - add branch multiplicacao

--- back.py
+++ back.py
@@ -5,3 +5,7 @@ class Subtracao():
 class Soma():
     def somar(valor1, valor2):
         return valor1 + valor2
+
+class Multiplicacao():
+    def multiplicar(valor1, valor2):
+        return valor1 * valor2
